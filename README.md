c17f36dd2ef31595dc75c56e49fc9b240b9579f1
심종훈 <sayhello.shim@gmail.com>
2021-04-19 00:19:18 +0900

    init setting

---
+++ README.md
@@ -0,0 +1,1 @@
+# g-nal_server
---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.use('/', (req, res) => {
+  res.send('Welcome to g-nal_server!');
+});
+
+app.listen(1324, () => {
+  console.log('server on 1324');
+});
