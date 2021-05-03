9bfbc4666c1267af52d1b810ef4d80879a013d7b
심종훈 <sayhello.shim@gmail.com>
2021-05-03 20:49:29 +0900

    [SERVER][FIX]users table
    users table 스키마 변경

--- models/user.js
+++ models/user.js
@@ -1,7 +1,5 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class user extends Model {
     /**
@@ -12,15 +10,17 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-  };
-  user.init({
-    email: DataTypes.STRING,
-    userName: DataTypes.STRING,
-    password: DataTypes.STRING,
-    mobile: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'user',
-  });
+  }
+  user.init(
+    {
+      userId: DataTypes.STRING,
+      nickname: DataTypes.STRING,
+      password: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: 'user',
+    },
+  );
   return user;
 };
