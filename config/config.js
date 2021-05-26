b58e1160e31a2387b49c34d04b4a83bfe621fdc6
심종훈 <sayhello.shim@gmail.com>
2021-05-26 23:28:46 +0900

    [SERVER][FIX]maintenance

--- config/config.js
+++ config/config.js
@@ -14,11 +14,11 @@ module.exports = {
     // },
   },
   test: {
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
+    username: 'root',
+    password: process.env.DATABASE_LOCAL_PASSWORD,
+    database: 'g-nal',
+    host: 'localhost',
+    port: '3306',
     dialect: 'mysql',
     timezone: '+09:00',
     // define: {
